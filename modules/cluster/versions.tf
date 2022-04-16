e475650cfd3fad3022d56b94c4fb6c71935a080f
Pavel Hrabec <77260724+Pavel-Hrabec@users.noreply.github.com>
2022-04-16 08:55:16 +0200

    Delete modules directory

--- modules/cluster/versions.tf
+++
@@ -1,9 +0,0 @@
-# Terraform
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.0.0"
-    }
-  }
-}
--- modules/network-security-groups/output.tf
+++
@@ -1,4 +0,0 @@
-output "nsg_id" {
-    description = "ID of the Network Security Group"
-    value = azurerm_network_security_group.nsg.id
-}
